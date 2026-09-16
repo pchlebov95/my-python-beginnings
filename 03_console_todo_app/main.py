f44729e98abd132adbded4131486829aa287647b
Patrik Chlebovčík <p.chlebovcik@email.cz>
2026-09-16 13:07:58 +0200

    Enhance error handling and task display in todo app
    Added error handling for user input in menu and delete_task functions. Improved task display formatting to show the longest task length.

--- 03_console_todo_app/main.py
+++ 03_console_todo_app/main.py
@@ -1,5 +1,4 @@
 memory = {}
-DASH = "-" * 21
 
 
 def add_task(task_dict: dict) -> None:
@@ -21,34 +20,45 @@ def show_task(task_dict: dict) -> None:
         print("\nYOUR TODO LIST IS EMPTY!")
     else:
         print("\n--- CURRENT TASKS ---")
-
+        max_length = 0
         for key, value in task_dict.items():
             print(f"{key}) {value}")
+            if len(value) > max_length:
+                max_length = len(value)
 
-        print(f"{DASH}")
+        print("-" * (max_length + 3))
 
 
 def delete_task(task_dict: dict) -> None:
     """Prompt the user to enter a task number and delete it from the dictionary."""
-    user_option = int(input("\nWHICH TASK YOU WANT DELETE?: "))
+    try:
+        user_option = int(input("\nWHICH TASK YOU WANT DELETE?: "))
+    except ValueError:
+        print("\nINVALID INPUT! PLEASE ENTER A NUMBER.")
+        return
 
     if user_option in task_dict:
-        del(task_dict[user_option])
+        del task_dict[user_option]
         print("\nTASK DELETED.")
     else:
         print("\nTASK NOT FOUND.")
 
 
 while True:
-    menu = input("\n1 = ADD TASK, 2 = SHOW TASKS, 3 = DELETE TASK, 4 = EXIT: ")
-    if menu == "1":
+    try:
+        menu = int(input("\n1 = ADD TASK, 2 = SHOW TASKS, 3 = DELETE TASK, 4 = EXIT: "))
+    except ValueError:
+        print("\nINVALID OPTION. PLEASE ENTER A NUMBER.")
+        continue
+
+    if menu == 1:
         add_task(memory)
-    elif menu == "2":
+    elif menu == 2:
         show_task(memory)
-    elif menu == "3":
+    elif menu == 3:
         delete_task(memory)
-    elif menu == "4":
-        print("EXIT...")
+    elif menu == 4:
+        print("EXIT..")
         break
     else:
         print("INVALID OPTION. PLEASE CHOOSE 1, 2, 3, OR 4.")
