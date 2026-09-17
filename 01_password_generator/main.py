841e566099e6c565f6cf6c54e257deae948daf29
Patrik Chlebovčík <p.chlebovcik@email.cz>
2026-09-17 17:01:06 +0200

    Improve password length validation with exception handling
    Refactored password length input handling to use exception for validation.

--- 01_password_generator/main.py
+++ 01_password_generator/main.py
@@ -1,40 +1,34 @@
-import string
 import secrets
+import string
 
 ALLOWED_CHARACTERS = string.ascii_letters + string.digits + string.punctuation
 
+
 def generate_length_password() -> int:
-    """
-    Ask the user for a password length and validate it.  
-    """
-
+    """Ask the user for a password length and validate it."""
     while True:
-        password_length = input("\nChoose your password length: ")
+        try:
+            password_length = int(input("\nChoose your password length: "))
 
-        if password_length.isdigit():
-            password_length_numb = int(password_length)
-
-            if password_length_numb >= 4:
-                return password_length_numb
+            if password_length >= 4:
+                return password_length
             else:
                 print("Password must be at least 4 characters long")
 
-        else:
+        except ValueError:
             print("Invalid input: Please enter a number.")
 
 
 def create_password(length: int) -> str:
-    """
-    Create a secure password from letters, digits, and punctuation.
-    """
-
+    """Create a secure password from letters, digits, and punctuation."""
     password_chars = []
 
     for _ in range(length):
         random_char = secrets.choice(ALLOWED_CHARACTERS)
         password_chars.append(random_char)
 
-    return ''.join(password_chars)
+    return "".join(password_chars)
+
 
 if __name__ == "__main__":
     while True:
